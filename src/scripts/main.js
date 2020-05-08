88cb669ccf50b0393316053b5a20a5c30275ab5f
Lyrklif <kochurow121@gmail.com>
2020-05-09 02:11:39 +0400

    54gybu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,6 +15,7 @@ import Modal from '../modules/modal/modal';
 import Film from '../modules/film/film';
 import Header from '../modules/header/header'; // настройка появления/скрытия header при скролле
 import Input from '../modules/input/input'; // при вводе текста в input placeholder поднимается
+import Checkbox from '../modules/checkbox/checkbox'; // checkbox
 import Tabs from '../modules/tabs/tabs'; // при вводе текста в input placeholder поднимается
 
 if (detection.isIE10Plus()) {
@@ -28,6 +29,7 @@ window.addEventListener('load', () => {
   new Header();
   new Burger();
   new Input();
+  new Checkbox();
   window.globalModal = new Modal();
   window.globalTabs = new Tabs();
   new Film();
