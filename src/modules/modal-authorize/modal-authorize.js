8da9b7973167e885cda9be05da78a1834bf890d9
Lyrklif <kochurow121@gmail.com>
2020-05-09 09:27:39 +0400

    52qyzd

--- src/modules/modal-authorize/modal-authorize.js
+++ src/modules/modal-authorize/modal-authorize.js
@@ -44,8 +44,8 @@ export default class ModalAuthorize {
   }
 
   writeData() {
-    const login = localStorage.getItem('login');
-    const password = localStorage.getItem('password');
+    const login = localStorage.getItem('videoservice_login');
+    const password = localStorage.getItem('videoservice_password');
 
     if (login) this.login.value = login;
     if (password) this.password.value = password;
@@ -57,8 +57,8 @@ export default class ModalAuthorize {
     const remember = this.remember.checked;
 
     if (remember) {
-      localStorage.setItem('login', login);
-      localStorage.setItem('password', password);
+      localStorage.setItem('videoservice_login', login);
+      localStorage.setItem('videoservice_password', password);
     }
   }
 }
--- src/modules/site-authorize/site-authorize.js
+++ src/modules/site-authorize/site-authorize.js
@@ -1,17 +1,102 @@
 'use strict';
-// site-authorize
 
-
-
+import { cssClasses } from '../../scripts/vars.js';
 
 export default class SiteAuthorize {
   constructor() {
-    this.wrappers = document.querySelectorAll('.js-site-authorize');
-    this.exitButtons = document.querySelectorAll('.js-sign-out');
-    this.authorize = false;
+    this.exitButtons = document.querySelectorAll('.js-sign-out-btn');
+
+    this.wrapSignIn = document.querySelectorAll('.js-sign-in-wrap');
+    this.wrapSignOut = document.querySelectorAll('.js-sign-out-wrap');
+
+    this.names = document.querySelectorAll('.js-name');
+    this.labels = document.querySelectorAll('.js-label-name');
+    this.inputs = document.querySelectorAll('.js-input-name');
+
+    if (this.names.length) {
+      this.writeName();
+    }
+
+    if (this.exitButtons.length) {
+      this.clickBtnExit();
+    }
+
+    if (this.names.length && this.labels.length && this.inputs.length) {
+      this.showInputChangeName();
+      this.changeName();
+    }
+
+    // cssClasses.showElem
+  }
+
+  // при нажатии на кнопки "выйти"
+  clickBtnExit() {
+    for (let i = 0; i < this.exitButtons.length; i++) {
+      this.exitButtons[i].addEventListener('click', () => {
+        this.exit();
+      });
+    }
+  }
+
+  // показать input для изменения имени
+  showInputChangeName() {
+    for (let i = 0; i < this.names.length; i++) {
+      this.names[i].addEventListener('click', () => {
+        this.hide(this.names[i]);
+        this.show(this.labels[i]);
+      });
+    }
+  }
+
+  // показать input для изменения имени
+  showTextName() {
+    for (let i = 0; i < this.names.length; i++) {
+      this.hide(this.labels[i]);
+      this.show(this.names[i]);
+    }
+  }
+
+  // изменить имя пользователя
+  changeName() {
+    for (let i = 0; i < this.inputs.length; i++) {
+      this.inputs[i].addEventListener('blur', () => {
+        const name = this.inputs[i].value;
+        localStorage.setItem('videoservice_name', name);
+        this.writeName();
+        this.showTextName();
+      });
+    }
+  }
+
+  writeName() {
+    const savedName = localStorage.getItem('videoservice_name');
+
+    if (savedName) {
+      for (let i = 0; i < this.names.length; i++) {
+        this.names[i].innerHTML = savedName;
+      }
+    }
   }
 
   authorize() {
-    this.authorize = true;
+    for (let i = 0; i < this.wrapSignIn.length; i++) {
+      this.hide(this.wrapSignIn[i]);
+      this.show(this.wrapSignOut[i]);
+    }
+  }
+
+  exit() {
+    for (let i = 0; i < this.wrapSignOut.length; i++) {
+      this.hide(this.wrapSignOut[i]);
+      this.show(this.wrapSignIn[i]);
+    }
+  }
+
+  hide(elem) {
+    elem.classList.remove(cssClasses.showElem);
+  }
+
+  show(elem) {
+    elem.classList.add(cssClasses.showElem);
   }
 }
